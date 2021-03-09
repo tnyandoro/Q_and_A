e8b4b42702408ad1ddb87a24bfe5e6572aee5201
Tendai Nyandoro <tendai1@hotmail.com>
2021-03-10 00:32:03 +0200

    Add the fonts and import to webpack bundle

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ function component() {
   // Lodash
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+   // Add the image to our existing div.
+   const myIcon = new Image();
+   myIcon.src = Icon;
+ 
+   element.appendChild(myIcon);
+ 
 
   return element;
 }
