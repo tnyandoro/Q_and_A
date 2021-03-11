8d5afc34d418f5bc831f34ccc39536a8b1cf7d18
Tendai Nyandoro <tendai1@hotmail.com>
2021-03-11 11:26:07 +0200

    Add Task function and the event Listener

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import _ from 'lodash';
 import './style.css';
 
-const addtaskinput = document.createElementId("addtaskinput");
+const addTaskInput = document.createElementId("addtaskinput");
+const addTaskBtn = document.createElementId("addtaskinput");
+
+addtaskinput.addEventListener("click", function(e) {
+  addTaskInputVal = addTaskInput.value;
+  let todoTask = localStorage.getItem("localTask");
+  if(todoTask === null) {
+    
+  }
+})
