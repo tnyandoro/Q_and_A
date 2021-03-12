7484ca5471b41bb851a81ab7405a3bc6da53e759
Tendai Nyandoro <tendai1@hotmail.com>
2021-03-12 13:20:52 +0200

    Create the header

--- src/index.js
+++ src/index.js
@@ -1,27 +1,2 @@
 import _ from 'lodash';
 import './style.css';
-
-let addtaskinput = document.getElementById("addtaskinput");
-let addtaskbtn = document.getElementById("addtaskbtn");
-
-addtaskbtn.addEventListener("click", function(){
-  addtaskinputval = addtaskinput.value;
-  let todoTask = localStorage.getItem("localtask");
-  if(todoTask == null){
-      taskObj = [];
-  }else{
-      taskObj = JSON.parse(todoTask);
-    }
-   taskObj.push(addtaskinputVal);
-   localStorage.setItem("localtask", JSON.stringify(taskObj));
-})
-function showTask(){
-  let todoTask = localStorage.getItem("localtask");
-  if(todoTask == null){
-      taskObj = [];
-  }else{
-      taskObj = JSON.parse(todoTask);
-    }
-}
-let html = '';
-let addedtasklist = getElementById("addedtasklist");
